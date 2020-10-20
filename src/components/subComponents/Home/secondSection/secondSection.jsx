4af91c6930bf5f935c9df400b3c1ec1bc30ca48f
hadeel salah <hadeelsalah80@gmail.com>
2020-10-21 01:11:27 +0300

    Create secondSection.jsx

---
+++ src/components/subComponents/Home/secondSection/secondSection.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../home.css';
+
+function SectionSection(){
+    return(
+            <div className="secondSection">
+               <h1>
+               Stay up to date in tech with
+               the online library for developers
+               </h1>
+               <p>
+               Access over 7,500+ practical books and videos on new
+               and emerging tech with a Packt subscription 
+               </p>
+               <button className="btn">
+                    START YOUR FREE trial
+               </button>
+            </div>
+           
+     )
+}
+export default SectionSection;
