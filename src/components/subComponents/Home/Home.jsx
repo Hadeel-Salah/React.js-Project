8dd0353fd8a942f8d7ad21ef76e08e6abc70f5e1
hadeel salah <hadeelsalah80@gmail.com>
2020-10-20 23:34:42 +0300

    Update Home.jsx

---
+++ src/components/subComponents/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import FirstSection from './firstSection/firstSection';
+import './home.css';
+function Home(){
+    return(
+        <div className="Home">
+           <FirstSection />
+        </div>
+     )
+}
+export default Home;
