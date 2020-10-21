34c2f42f2736f80abe88c1f11f5e6f98ff60259c
hadeel salah <hadeelsalah80@gmail.com>
2020-10-21 20:36:44 +0300

    add section four

--- src/components/subComponents/Home/Home.jsx
+++ src/components/subComponents/Home/Home.jsx
@@ -3,12 +3,14 @@ import FirstSection from './firstSection/firstSection';
 import SecondSection from './secondSection/secondSection'
 import './home.css';
 import ThirdSection from './thirdSection/thirdSection';
+import FourthSection from './FourthSection/fourthSection';
 function Home(){
     return(
         <div className="Home">
            <FirstSection />
            <SecondSection />
            <ThirdSection />
+           <FourthSection />
         </div>
      )
 }
