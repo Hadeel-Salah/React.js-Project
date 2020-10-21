7a55507b4c6f8eb17a45179da3a9c7c5cc308ce9
hadeel salah <hadeelsalah80@gmail.com>
2020-10-21 20:37:53 +0300

    build section four

---
+++ src/components/subComponents/Home/FourthSection/fourthSection.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function FourthSection(){
+    return(
+        <div className="FourthSection">
+            <div class="row">
+            <div class="column">
+                <img src="https://www.packtpub.com/media/wysiwyg/GLOBALCON4-BANNERS_576x226_002_.png" alt="book" />
+            </div>
+            <div class="column">
+                <img src="https://www.packtpub.com/media/wysiwyg/ray_summit_576X226.png" alt="book" />
+            </div>
+            </div>
+        </div>        
+    )
+}
+export default FourthSection;
