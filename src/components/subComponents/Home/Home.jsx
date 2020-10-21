567ff808a49780711f6d373fd1125f84c6ab0fc1
hadeel salah <hadeelsalah80@gmail.com>
2020-10-21 17:31:00 +0300

    Update Home.jsx

--- src/components/subComponents/Home/Home.jsx
+++ src/components/subComponents/Home/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import FirstSection from './firstSection/firstSection';
 import SecondSection from './secondSection/secondSection'
 import './home.css';
+import ThirdSection from './thirdSection/thirdSection';
 function Home(){
     return(
         <div className="Home">
            <FirstSection />
            <SecondSection />
+           <ThirdSection />
         </div>
      )
 }
