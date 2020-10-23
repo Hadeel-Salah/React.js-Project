190cc7b3cea6be77bc93f3ae945d3beb4e22aa21
hadeel salah <hadeelsalah80@gmail.com>
2020-10-23 15:27:10 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Header from './components/layout/header/header.jsx';
 import './App.css';
 import Footer from './components/layout/footer/footer.jsx';
 import Home from './components/subComponents/Home/Home'
+import Details from './components/subComponents/BooksDetails/details.jsx';
 function App() {
   return (
-    <>
-        <Header />
-          <Home />
-        <Footer />
-    </>
+      <>
+        <Router>
+          <Header />
+                <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route exact path ="/book/:id"  component={Details} />
+                </Switch> 
+            <Footer/>
+          </Router>  
+      </>
   );
 }
 
